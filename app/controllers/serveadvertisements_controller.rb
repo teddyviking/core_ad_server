8c6e96863b3c94507fbc595ed331ff721e2748a9
Rafael Tejado <rtejado90@gmail.com>
2015-06-02 15:44:48 +0200

    Add access control to response

--- app/controllers/serveadvertisements_controller.rb
+++ app/controllers/serveadvertisements_controller.rb
@@ -1,11 +1,12 @@
 class ServeadvertisementsController < ApplicationController
 
-	 def process_request
-	 	render :json => { newAdvertisement: get_advertisement }
-	 end
+   def process_request
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    render :json => { newAdvertisement: get_advertisement }
+   end
 
-	 def get_advertisement
-	 	return url = 'https://www.elcorteingles.es/sgfm/SGFM/contents/images/uploads/2015/06/556c7139e5dd8e3226b25687.jpg'
-	 end
+   def get_advertisement
+    return url = 'https://www.elcorteingles.es/sgfm/SGFM/contents/images/uploads/2015/06/556c7139e5dd8e3226b25687.jpg'
+   end
 
 end
