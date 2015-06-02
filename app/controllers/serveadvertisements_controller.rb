65a5a8d42782113d7dcf685b74fc95f0d23036c7
Juan Antona <juan.antonadelval@gmail.com>
2015-06-02 17:59:10 +0200

    manage account id

--- app/controllers/serveadvertisements_controller.rb
+++ app/controllers/serveadvertisements_controller.rb
@@ -5,15 +5,19 @@ class ServeadvertisementsController < ApplicationController
 
     account_id = get_account_id(params)
 
-    render :json => { newAdvertisement: get_advertisement 
-                      # accountId: account_id
+    render :json => { newAdvertisement: get_advertisement(account_id), 
+                      accountId: account_id
                     }
 
    end
 
-   def get_advertisement
-    
-    return url = 'https://www.elcorteingles.es/sgfm/SGFM/contents/images/uploads/2015/06/556c7139e5dd8e3226b25687.jpg'
+   def get_advertisement(account_id)
+
+   	if account_id == 1
+      url = 'https://www.elcorteingles.es/sgfm/SGFM/contents/images/uploads/2015/06/556c7139e5dd8e3226b25687.jpg'
+    elsif account_id == 2
+      url = 'https://www.elcorteingles.es/sgfm/SGFM/contents/images/uploads/2015/06/556c5b1791fa3d2926078970.jpg'
+    end   
   
    end
 
