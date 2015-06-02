2b203274113bec3c084292fb4acb13c9322008ce
Juan Antona <juan.antonadelval@gmail.com>
2015-06-02 16:02:45 +0200

    response account id

--- app/controllers/serveadvertisements_controller.rb
+++ app/controllers/serveadvertisements_controller.rb
@@ -6,9 +6,10 @@ class ServeadvertisementsController < ApplicationController
     account_id = get_account_id(params)
 
     render :json => { newAdvertisement: get_advertisement, accountId: account_id }
+
    end
 
-   def get_advertisement(params)
+   def get_advertisement
     
     return url = 'https://www.elcorteingles.es/sgfm/SGFM/contents/images/uploads/2015/06/556c7139e5dd8e3226b25687.jpg'
   
