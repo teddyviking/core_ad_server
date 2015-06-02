7c33633f5d09f7dbb21bff0bcec6373649ca4f79
Juan Antona <juan.antonadelval@gmail.com>
2015-06-02 19:21:22 +0200

    load js file

--- app/controllers/serveadvertisements_controller.rb
+++ app/controllers/serveadvertisements_controller.rb
@@ -1,31 +1,37 @@
 class ServeadvertisementsController < ApplicationController
 
    def process_request
-    response.headers["Access-Control-Allow-Origin"] = "*"
+	    response.headers["Access-Control-Allow-Origin"] = "*"
 
-    account_id = get_account_id(params)
+	    account_id = get_account_id(params)
 
-    render :json => { newAdvertisement: get_advertisement(account_id), 
-                      accountId: account_id
-                    }
+	    render :json => { newAdvertisement: get_advertisement(account_id), 
+	                      accountId: account_id
+	                    }
 
    end
 
    def get_advertisement(account_id)
 
-   	if account_id == 1
-      url = 'https://www.elcorteingles.es/sgfm/SGFM/contents/images/uploads/2015/06/556c7139e5dd8e3226b25687.jpg'
-    elsif account_id == 2
-      url = 'https://www.elcorteingles.es/sgfm/SGFM/contents/images/uploads/2015/06/556c5b1791fa3d2926078970.jpg'
-    end   
+	   if account_id == 1
+	      url = 'https://www.elcorteingles.es/sgfm/SGFM/contents/images/uploads/2015/06/556c7139e5dd8e3226b25687.jpg'
+	   elsif account_id == 2
+	      url = 'https://www.elcorteingles.es/sgfm/SGFM/contents/images/uploads/2015/06/556c5b1791fa3d2926078970.jpg'
+	   end   
   
+   end
+
+   def load_js
+
+   	render 'ajax'
+   	
    end
 
    private
 
    def get_account_id(params)
 
-    params[:account_id].to_i
+    	params[:account_id].to_i
     
    end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
 
 	get '/advertisements/:account_id' => 'serveadvertisements#process_request'
 
+	get '/advertisements/load_js' => 'serveadvertisements#load_js'
+
 end
