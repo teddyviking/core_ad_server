3bfc151d0f6949e132064c73fe643dbc7735ba3d
Juan Antona <juan.antonadelval@gmail.com>
2015-06-03 10:03:36 +0200

    request form rails

--- app/controllers/serveadvertisements_controller.rb
+++ app/controllers/serveadvertisements_controller.rb
@@ -1,5 +1,11 @@
 class ServeadvertisementsController < ApplicationController
 
+require "uri"
+require "net/http"
+
+
+
+
    def process_request
 	    response.headers["Access-Control-Allow-Origin"] = "*"
 
@@ -13,11 +19,16 @@ class ServeadvertisementsController < ApplicationController
 
    def get_advertisement(account_id)
 
-	   if account_id == 1
-	      url = 'https://www.elcorteingles.es/sgfm/SGFM/contents/images/uploads/2015/06/556c7139e5dd8e3226b25687.jpg'
-	   elsif account_id == 2
-	      url = 'https://www.elcorteingles.es/sgfm/SGFM/contents/images/uploads/2015/06/556c5b1791fa3d2926078970.jpg'
-	   end   
+   	   params = {'user_id' => account_id,'button1' => 'Submit'}
+
+       x = Net::HTTP.post_form(URI.parse('//10.28.30.59:3000/metrics'), params)
+       puts x.body
+
+	   # if account_id == 1
+	   #    url = 'https://www.elcorteingles.es/sgfm/SGFM/contents/images/uploads/2015/06/556c7139e5dd8e3226b25687.jpg'
+	   # elsif account_id == 2
+	   #    url = 'https://www.elcorteingles.es/sgfm/SGFM/contents/images/uploads/2015/06/556c5b1791fa3d2926078970.jpg'
+	   # end   
   
    end
 
